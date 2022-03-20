b705592827099a3e4e80056ac6bcb1c7aa02bd2e
Mark Brock <Markbrock@users.noreply.github.com>
2022-03-20 18:29:31 -0400

    auth

--- bugtracker_react/src/App.js
+++ bugtracker_react/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+import Amplify, { Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+const api = axios.create({
+  baseURL: 'https://w3iyzol7mi.execute-api.us-east-1.amazonaws.com'
+
+})
+export default function App() {
+
+  const [Bugs,setBugs] = useState([])
+  const [currentBug,setcurrentBug] = useState([])
+  React.useEffect(() => {
+    api.get('/Dev/bugtracker').then((response) => {
+    console.log(response.data.attributes)
+    setBugs(response.data)  
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          
+          <h1>Hello {user.attributes.preferred_username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
